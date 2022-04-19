16daa146c3baf6d6424584c347c5d899c3fbec47
unknown <LeonCelestino@outlook.com>
2022-04-19 16:22:15 -0300

    Changed to english
    I changed some text from portuguese  to english

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var país = document.querySelector("input#country")
+var country = document.querySelector("input#country")
 var click = document.querySelector(".box");
 
 function clicar()
 {
-    click.innerHTML=`Você nasceu no ${país.value}!`
+    click.innerHTML=`You're from ${country.value}!`
     click.style.fontSize = "xx-large"
 }
