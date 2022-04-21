86c20faed3fe06905d5141a25fc6a519f3049d7c
LeonCelestino <88250273+LeonCelestino@users.noreply.github.com>
2022-04-21 12:20:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var country = document.querySelector("input#country")
 var click = document.querySelector(".box");
 
-function click()
+function clickEvent()
 {
     click.innerHTML=`You're from ${country.value}!`
     click.style.fontSize = "xx-large"
